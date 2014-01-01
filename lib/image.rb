c71df94513d6c2fb7de5f1aef61c65558aed7ff5
Jean-Baptiste Blanc <jbblanc@gmail.com>
2014-01-01 22:10:37 +0100

    cell coloring and adjacent cell finding

--- lib/image.rb
+++ lib/image.rb
@@ -1,14 +1,14 @@
 class Image
 
 	def initialize(m,n)
-		@pixels = Array.new(m){Array.new(n){'O'}}
-	end
-
-	def m
-		@pixels.size
+		@pixels = Array.new(n){Array.new(m){'O'}}
 	end
 
 	def n
+		@pixels.size
+	end
+
+	def m
 		@pixels.first.size
 	end
 
@@ -17,5 +17,22 @@ class Image
 		(x <= m && x >=0) && (y <= n && x >=0)
 	end
 
+	def color_pixel!(pixel_coords,color)
+		@pixels[pixel_coords[1] -1][pixel_coords[0] -1] = color	
+	end
 
+	def pixel_color(pixel_coords)
+		@pixels[pixel_coords[1] -1][pixel_coords[0] -1]
+	end
+
+	def adjacent_cell_with_same_color(pixel_coords)
+		color = pixel_color(pixel_coords)
+		x, y = pixel_coords
+		cells = [[x-1,y],[x+1,y],[x,y-1],[x,y+1]]
+		cells.select{|cell| pixel_color(cell) == color && contain?(cell)}.sort
+	end
+
+	def to_s
+		@pixels.map { |row| "#{row.join}\n" }.join
+	end
 end
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -9,12 +9,43 @@ describe Image do
 		end
 	end
 
-	context 'Action' do
-		it 'should know if a pixel is contained in an image' do
+	context 'knowledge' do
+		it 'should know the belonging of a pixel  in an image' do
 				expect(image_4_by_6.contain?([3,4])).to be_true
 				expect(image_4_by_6.contain?([5,5])).to be_false
 				expect(image_4_by_6.contain?([3,8])).to be_false
 				expect(image_4_by_6.contain?([8,8])).to be_false
 		end
+
+		it 'should know how to color of a pixel' do
+			image_4_by_6.color_pixel!([3,4],'A')
+			expect(image_4_by_6.pixel_color([3,4])).to eq('A')				
+		end		
+	end
+
+	context 'presentation' do
+		 it 'is represented as an M x N array of pixels set to \'O\'' do
+		 	expect(image_4_by_6.to_s).to eq("OOOO\nOOOO\nOOOO\nOOOO\nOOOO\nOOOO\n")
+		 end
+
+		 it 'should color a pixel at a defined coordinates' do
+		 	image_4_by_6.color_pixel!([3,4],'A')
+		 	expect(image_4_by_6.to_s).to eq("OOOO\nOOOO\nOOOO\nOOAO\nOOOO\nOOOO\n")
+		 end
+
+		 it 'should find adjacent pixels with the same color' do
+		 	image_4_by_6.color_pixel!([3,4],'B')
+		  expect(image_4_by_6.adjacent_cell_with_same_color([3,4])).to eq([])
+		  image_4_by_6.color_pixel!([2,4],'B')
+		  expect(image_4_by_6.adjacent_cell_with_same_color([3,4])).to eq([[2,4]])
+		  image_4_by_6.color_pixel!([1,2],'B')
+		  expect(image_4_by_6.adjacent_cell_with_same_color([3,4])).to eq([[2,4]])
+		  image_4_by_6.color_pixel!([3,3],'B')
+		  expect(image_4_by_6.adjacent_cell_with_same_color([3,4])).to eq([[2,4],[3,3]])
+		  image_4_by_6.color_pixel!([3,5],'B')
+		  expect(image_4_by_6.adjacent_cell_with_same_color([3,4])).to eq([[2,4],[3,3],[3,5]])
+		  image_4_by_6.color_pixel!([4,4],'B')
+		  expect(image_4_by_6.adjacent_cell_with_same_color([3,4])).to eq([[2,4],[3,3],[3,5],[4,4]])
+			end
 	end
 end
